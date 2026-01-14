18ad34b2f59bf811e0e33f2335263be86880a666
Lucas <lucassilvadpaula@gmail.com>
2026-01-13 23:38:27 -0300

    feature: add new hosts to acess the server

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: ['http://localhost:3000','https://casanova.bgtech.com.br','https://nossolar.bgtech.com.br'],
     credentials: true, // use se estiver usando cookies/auth
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
